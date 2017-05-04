23663876498cea5359a9aaf3c93bda36296ab740
teste <teste@teste.teste>
2017-05-04 09:37:12 -0300

    Novo commit

---
+++ src/Outros/OutroTeste.java
@@ -0,0 +1,11 @@
+package Outros;
+
+/**
+ *
+ * @author Tester
+ */
+public class OutroTeste {
+    public static void fazAlgo () {
+        System.out.println(System.currentTimeMillis());
+    }
+}
---
+++ src/Testes/Teste.java
@@ -0,0 +1,14 @@
+package Testes;
+
+import static Outros.OutroTeste.fazAlgo;
+
+/**
+ *
+ * @author Tester
+ */
+public class Teste {
+    public static void main (String[] args) {
+        fazAlgo();
+        System.exit(0);   
+    }
+}
